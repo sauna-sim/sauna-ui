c3a9307e97f110fd934e490fabcfc84cca5ec1fd
Prithvi Shivaraman <prithvisagar.shivaraman@gmail.com>
2023-09-22 16:15:44 -0400

    Reverted FMA stuff

--- src/pages/aircraft/aircraft.js
+++ src/pages/aircraft/aircraft.js
@@ -41,75 +41,22 @@ export class AircraftPage extends Component {
         }
     }
 
-    getArmedModes = (armedModes) => {
-        if (!armedModes){
-            return "";
-        }
-        let armedStr = "";
-        for (const armedMode in armedModes){
-            armedStr += `${armedMode} `;
-        }
-
-        return armedStr;
-    }
-
-    getFma = (aircraft) => {
-        return <>
-            <Row>
-
-            </Row>
-            <Row>
-                <Col className={"fma-active-conv"}>{aircraft.autopilot.currentThrustMode}</Col>
-                <Col className={"fma-active-conv"}>{aircraft.autopilot.currentLateralMode}</Col>
-                <Col className={"fma-active-conv"}>{aircraft.autopilot.currentVerticalMode}</Col>
-            </Row>
-            <Row>
-                <Col className={"fma-armed"}>{this.getArmedModes(aircraft.autopilot.armedThrustModes)}</Col>
-                <Col className={"fma-armed"}>{this.getArmedModes(aircraft.autopilot.armedLateralModes)}</Col>
-                <Col className={"fma-armed"}>{this.getArmedModes(aircraft.autopilot.armedVerticalModes)}</Col>
-            </Row>
-        </>
-    }
-
     getAircraftTable = () => {
         return this.state.aircraftList.map((aircraft) => {
             return <tr key={aircraft.callsign}>
                 <td>{aircraft.callsign}</td>
-                <td>
-                    <div>{aircraft.connectionStatus}</div>
-                    <div>{aircraft.paused ? "Paused" : "Unpaused"}</div>
-                </td>
-                <td>
-                    <div className={"pfd-selected"}>{aircraft.autopilot.selectedHeading}</div>
-                    <div className={"pfd-measured"}>{round(aircraft.position.heading_Mag, 2)}</div>
-                </td>
-                <td>
-                    <div className={aircraft.autopilot.selectedSpeedMode === "MANUAL" ? "pfd-selected" : "pfd-managed"}>
-                        {aircraft.autopilot.selectedSpeedUnits === "KNOTS" ?
-                            aircraft.autopilot.selectedSpeed :
-                            `M${round(aircraft.autopilot.selectedSpeed / 100.0, 2)}`}
-                    </div>
-                    <div className={"pfd-measured"}>{round(aircraft.position.indicatedAirSpeed, 2)}</div>
-                </td>
-                <td>
-                    <div className={"pfd-selected"}>{aircraft.autopilot.selectedAltitude}</div>
-                    <div className={"pfd-measured"}>{round(aircraft.position.indicatedAltitude, 2)}</div>
-                </td>
-                <td>
-                    <div className={"pfd-selected"}>{aircraft.autopilot.selectedVerticalSpeed}</div>
-                    <div className={"pfd-measured"}>{round(aircraft.position.verticalSpeed, 2)}</div>
-                </td>
-                <td>
-                    <div className={"pfd-selected"}>{aircraft.autopilot.selectedFpa}</div>
-                    <div className={"pfd-measured"}></div>
-                </td>
-                <td>{this.getFma(aircraft)}</td>
-                <td>{round(aircraft.position.pitch, 1)}</td>
-                <td>{round(aircraft.position.bank, 2)}</td>
-                <td>{round(aircraft.thrustLeverPos, 2)}</td>
+                <td>{aircraft.connectionStatus}</td>
+                <td>{aircraft.paused ? "Paused" : "Unpaused"}</td>
+                <td>{round(aircraft.position.heading_Mag)}</td>
+                <td>{round(aircraft.position.indicatedAirSpeed)}</td>
+                <td>{round(aircraft.position.indicatedAltitude)}</td>
                 <td>{`${round(aircraft.position.altimeterSetting_hPa)}hPa`}</td>
                 <td>{`${round(aircraft.position.windDirection)} @ ${round(aircraft.position.windSpeed)}kts`}</td>
-
+                <td>{aircraft.control.currentLateralModeStr}</td>
+                <td>{aircraft.control.armedLateralModeStr}</td>
+                <td>{aircraft.control.currentVerticalModeStr}</td>
+                <td>{aircraft.control.armedVerticalModesStr}</td>
+                <td>{aircraft.control.fms.asString}</td>
             </tr>
         })
     }
@@ -123,22 +70,22 @@ export class AircraftPage extends Component {
                     <Button variant="outline-success" className="me-2" onClick={() => unpauseAllAircraft()}>Unpause All</Button>
                     <Button variant="danger" className="me-2" onClick={() => removeAllAircraft()}>Delete All</Button>
                 </ButtonToolbar>
-                <Table striped bordered hover size={"sm"}>
+                <Table striped bordered hover>
                     <thead>
                     <tr>
                         <th>Callsign</th>
-                        <th>Status</th>
-                        <th>Heading</th>
-                        <th>Airspeed</th>
-                        <th>Altitude</th>
-                        <th>V/S</th>
-                        <th>FPA</th>
-                        <th>FMA</th>
-                        <th>Pitch</th>
-                        <th>Bank</th>
-                        <th>Thrust</th>
-                        <th>Baro</th>
+                        <th>Connection</th>
+                        <th>Paused</th>
+                        <th>Heading (Magnetic)</th>
+                        <th>Airspeed (KIAS)</th>
+                        <th>Altitude (ft)</th>
+                        <th>Altimeter Setting</th>
                         <th>Wind</th>
+                        <th>Lateral Mode</th>
+                        <th>Armed Lateral Mode</th>
+                        <th>Vertical Mode</th>
+                        <th>Armed Vertical Mode</th>
+                        <th>FMS Route</th>
                     </tr>
                     </thead>
                     <tbody>
