c5c6da68d96ccaa3b6400e5ad978a41d65a51ceb
Prithvi Shivaraman <prithvisagar.shivaraman@gmail.com>
2023-10-24 18:38:40 -0400

    More map updates

--- src/index.js
+++ src/index.js
@@ -3,20 +3,16 @@ import {createRoot} from "react-dom/client";
 import './assets/stylesheets/index.scss';
 import 'leaflet/dist/leaflet.css';
 import MainApp from "./pages/main";
-import {store} from "./redux/store";
-import {Provider} from "react-redux";
 import {HashRouter, Route, Routes} from "react-router-dom";
 import {MapPage} from "./pages/map/map_page";
 
 // React DOM Render
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
-        <HashRouter>
-            <Routes>
-                <Route path="/" element={<MainApp/>}/>
-                <Route path="/map" element={<MapPage/>}/>
-            </Routes>
-        </HashRouter>
-    </Provider>
+    <HashRouter>
+        <Routes>
+            <Route path="/" element={<MainApp/>}/>
+            <Route path="/map" element={<MapPage/>}/>
+        </Routes>
+    </HashRouter>
 );
