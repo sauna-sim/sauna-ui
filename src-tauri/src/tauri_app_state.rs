e9eb3626b5ad81d8f4c54d94f427216b5d73ede9
Prithvi Shivaraman <prithvisagar.shivaraman@gmail.com>
2024-11-07 17:20:11 -0500

    Version bump for API update.

--- src-tauri/src/tauri_app_state.rs
+++ src-tauri/src/tauri_app_state.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 use crate::child_guard::ChildGuard;
-use crate::local_store::{StoreContainer};
+use crate::local_store::StoreContainer;
 use crate::utils::port_finder::get_available_port;
 
 
