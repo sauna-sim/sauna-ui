52e821c8ffdd2631aeed8124e7935c3f44679d9a
Odias <odiasukcamaj@gmail.com>
2024-11-21 02:32:27 +0000

    Started work on sauna scenario file maker

--- src/index.jsx
+++ src/index.jsx
@@ -5,14 +5,17 @@ import MainApp from "./pages/main";
 import {store} from "./redux/store";
 import {Provider} from "react-redux";
 import {HashRouter, Route, Routes} from "react-router-dom";
+import SaunaScenarioMaker from './pages/scenario_maker/sauna_scenario_maker';
 
 // React DOM Render
+//Add route to comp
 const root = createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
         <HashRouter>
             <Routes>
                 <Route path="/" element={<MainApp />}/>
+                <Route path="sauna_scenario_maker" element={<SaunaScenarioMaker />}/>
             </Routes>
         </HashRouter>
     </Provider>
---
+++ src/pages/scenario_maker/sauna_scenario_maker.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+
+import Form from 'react-bootstrap/Form';
+
+export default function SaunaScenarioMaker() {
+    
+    return (
+        <div></div>
+    );
+}
